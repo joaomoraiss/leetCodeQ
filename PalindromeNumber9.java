1501797bcdc66311b3d0d0d6443a39cde311c996
joaomoraiss <joaovictorrmpework@outlook.com>
2024-09-24 20:28:34 -0300

    PalindromeNumber9 done

---
+++ PalindromeNumber9.java
@@ -0,0 +1,13 @@
+public class PalindromeNumber9 {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+    }
+    public static boolean isPalindrome(int x) {
+        StringBuilder str = new StringBuilder(Integer.toString(x));
+        StringBuilder str2 = new StringBuilder(str);
+        str2.reverse();
+
+        if (str2.toString().equals(str.toString()))return true;
+        else return false;
+    }
+}
