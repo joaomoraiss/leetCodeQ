eebef8e03e0710f05367483447a8467d4edc75c6
joaomoraiss <joaovictorrmpework@outlook.com>
2024-09-24 18:59:27 -0300

    RomanToInteger13 fix

--- RomanToInteger13.java
+++ RomanToInteger13.java
@@ -7,18 +7,18 @@ public class RomanToInteger13 {
         System.out.println(romanToInt("MCMXCIV"));
     }
     public static int romanToInt(String s){
-        HashMap<Character, Integer> romanValue = new HashMap<>();
+        HashMap<Character, Integer> romanValues = new HashMap<>();
         int valor = 0;
-        romanValue.put('I', 1);
-        romanValue.put('V', 5);
-        romanValue.put('X', 10);
-        romanValue.put('L', 50);
-        romanValue.put('C', 100);
-        romanValue.put('D', 500);
-        romanValue.put('M', 1000);
+        romanValues.put('I', 1);
+        romanValues.put('V', 5);
+        romanValues.put('X', 10);
+        romanValues.put('L', 50);
+        romanValues.put('C', 100);
+        romanValues.put('D', 500);
+        romanValues.put('M', 1000);
         for (int i = 0 ; i < s.length(); i++){
-            int curretValue = romanValue.get(s.charAt(i));
-            if (i + 1 < s.length() && romanValue.get(s.charAt(i+1)) > curretValue)valor -=curretValue;
+            int curretValue = romanValues.get(s.charAt(i));
+            if (i + 1 < s.length() && romanValues.get(s.charAt(i+1)) > curretValue)valor -=curretValue;
             else valor += curretValue;
         }
     return valor;
