68b64ef6564a9881533f5d8f0e9364ad081e374c
joaomoraiss <joaovictorrmpework@outlook.com>
2024-09-25 13:14:16 -0300

    BinarySearch

---
+++ BinarySearch.java
@@ -0,0 +1,18 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int arr[] = new int[]{1,2,3,4,5,6,7};
+        System.out.println(binarySearch(7, arr));
+    }
+    public static int binarySearch(int target, int arr[]){
+        int low = 0;
+        int high = arr.length -1;
+
+        while (low <= high){
+            int middlePosition = (low + high)/2;
+            if (arr[middlePosition] == target)return middlePosition;
+            if (arr[middlePosition] > target) high = middlePosition -1;
+            else low = middlePosition +1;
+        }
+        return -1;
+    }
+}
