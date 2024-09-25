055f5a4e57066cccd454058ac3ac5191e36546e8
joaomoraiss <joaovictorrmpework@outlook.com>
2024-09-25 12:08:34 -0300

    MaximumSubArray53 done

---
+++ MaximumSubArray53.java
@@ -0,0 +1,17 @@
+public class MaximumSubArray53 {
+    public static void main(String[] args) {
+        int nums[] = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+            if (currentSum > maxSum) maxSum = currentSum;
+            if (currentSum < 0) currentSum = 0;
+        }
+        return maxSum;
+    }
+}
