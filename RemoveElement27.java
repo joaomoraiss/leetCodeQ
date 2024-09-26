9eee8298cc051cd613dd1d30f688f361a3a3f5be
joaomoraiss <joaovictorrmpework@outlook.com>
2024-09-26 10:42:45 -0300

    RemoveElement27 done

---
+++ RemoveElement27.java
@@ -0,0 +1,16 @@
+public class RemoveElement27 {
+    public static void main(String[] args) {
+        int arr[] = new int[]{3,2,2,3};
+        System.out.println(removeElement(arr, 3));
+    }
+    public static int removeElement(int[] nums, int val) {
+        int valOccorurrence = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] != val){
+                nums[valOccorurrence] = nums[i];
+                valOccorurrence++;
+            }
+        }
+        return valOccorurrence;
+    }
+}
