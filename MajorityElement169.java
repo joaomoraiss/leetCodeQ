2494821a90ce720af843980d1e2a0390074c7066
joaomoraiss <joaovictorrmpework@outlook.com>
2024-09-28 09:48:27 -0300

    solução 1 MajorityElement169

--- MajorityElement169.java
+++ MajorityElement169.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class MajorityElement169 {
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class MajorityElement169 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{2,2,1,1,1,2,2};
+        System.out.println(majorityElement(nums));
+    }
+    public static int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> majority = new HashMap<>();
+        int maisRepetido = 0;
+        int elementoMaior = 0;
+        for(int i = 0; i < nums.length; i++){
+            majority.put(nums[i], majority.containsKey(nums[i]) ? majority.get(nums[i]) + 1 : 1);
+        }
+        for (int key : majority.keySet()){
+            if (majority.get(key) > maisRepetido){
+                maisRepetido = majority.get(key);
+                elementoMaior = key;
+            }
+        }
+        return elementoMaior;
+    }
 }
